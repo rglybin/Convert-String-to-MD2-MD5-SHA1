5892aa2ce94ce4198f6a6fbc0c62a3ac69243295
Ruslan Glybin <rglybin@gmail.com>
2017-08-30 19:29:25 -0400

    Add files via upload

---
+++ JavaPassword.java
@@ -0,0 +1,30 @@
+
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class JavaPassword {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//String salt = "RandomStringForExtraSecurity@#$!%^&*(*)1234567890";
+		String unecryptedPassword = "m2vqc5d3";
+		System.out.println("Unencrypted Password: " + unecryptedPassword);
+		
+		MessageDigest messageDigest=null;
+		try {
+			messageDigest = MessageDigest.getInstance("SHA1");
+			//messageDigest = MessageDigest.getInstance("MD5");
+			//messageDigest = MessageDigest.getInstance("MD2");
+			messageDigest.update((unecryptedPassword).getBytes());
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		String encryptedPassword = (new BigInteger(messageDigest.digest())).toString(16);
+		System.out.println("Encrypted Password: " + encryptedPassword);
+	}
+
+}
